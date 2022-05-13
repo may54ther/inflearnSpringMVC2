9e1aaa59334b15098ed9e6e08a0fe5be4af2478b
ahakim <aha9654@gmail.com>
2022-05-13 20:07:33 +0900

    add: API 오류 - 서블릿 오류 페이지 방식

--- exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -6,7 +6,7 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component //서블릿 방식 사용시 주석
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
     @Override
