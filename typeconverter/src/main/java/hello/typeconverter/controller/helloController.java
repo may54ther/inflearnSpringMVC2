cb964eea21286c8578ce414c31cf63b27ab68a5d
ahakim <aha9654@gmail.com>
2022-05-15 18:39:47 +0900

    add: 요청 파라미터 형변환

---
+++ typeconverter/src/main/java/hello/typeconverter/controller/helloController.java
@@ -0,0 +1,19 @@
+package hello.typeconverter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class helloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data");
+        Integer intValue = Integer.valueOf(data);
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+}
